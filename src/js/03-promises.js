82a7a6c90430a3ded1b4e7262bbb2a6658e06bd9
illya15 <113913094+illya15@users.noreply.github.com>
2023-03-18 00:09:48 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmitForm);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
